3e5857d57a602ac72b9c9e0e65b675b8be3640fc
Zmoore <zmoore371@gmail.com>
2021-10-26 11:25:41 -0400

    Created prompts for most functions, need to add logic and figure out promises

--- program.js
+++ program.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const consoleTable = require("console.table")
+const { restoreDefaultPrompts } = require("inquirer");
 
 
 
@@ -30,12 +32,9 @@ db.connect((err) => {
         if(err) {
             console.log(err)
         }
-        console.log(result)
+        // console.log(result)
         mainMenu();
     })
-
-    //put a function here to start program after successfully connecting
-
 });
 
 mainMenu = () => {
@@ -43,6 +42,7 @@ mainMenu = () => {
         .prompt([
             {
             type: 'list',
+            message: "What would you like to do?",
             name: 'operationSelection',
             choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"]
             }
@@ -52,22 +52,22 @@ mainMenu = () => {
             
             switch(answer.operationSelection) {
                 case "View All Employees":
-                    console.log("Hello")
+                    viewAllEmployees();
                     break;
                 case "Add Employee":
-
+                    addEmployee();
                     break;  
                 case "Update Employee Role":
 
                     break;  
                 case "View All Roles":
-
+                    viewAllRoles();
                     break;  
                 case "Add Role":
 
                     break;
                 case "View All Departments":
-
+                    viewAllDepartments();
                     break;
                 case "Add Department":
 
@@ -80,3 +80,116 @@ mainMenu = () => {
                 
         }) 
 }    
+
+// viewAllEmployees = () => {
+    
+//     db.query("SELECT * FROM employee", (err, results) => {
+//         if(err){
+//             console.log(err)
+//         }
+//         return console.table(results)
+//     })
+//     setTimeout (() => {
+//     mainMenu()
+//     },2000)
+// }
+
+const viewAllEmployees = () => {
+    let query = ("SELECT * FROM employee")
+
+    db.promise().query(query, (err, result) => {
+        if(err) throw err;
+
+        console.table(result)
+    })
+}
+
+addEmployee = () => {
+    // query db for list of avaliable roles to insert into question below
+    
+    
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is the employees first name?"
+            },
+            {
+                type: "input", 
+                name: "last_name",
+                message: "What is the employees last name?"
+            },
+            {
+                type: "list",
+                name: "role",
+                message: "INSERT ROLES TO CHOSE FROM HERE"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What is the employees yearly salary?"
+            },
+            {
+                type: "confirm",
+                name: "isManager",
+                message: "Is this person a manager?",
+                default: false
+            }
+        ])
+        // add check for manager or not here then insert everything into corresponding fields
+}
+
+updateRole = () => {
+    //get all employees to list
+    // get all avaliable roles to list 
+    
+    inquirer
+        .prompt([
+            {
+            type: "list",
+            name: "selectedEmployee",
+            message: "Chose employee to update role:",
+            choices: "CHOICE ARRAY"
+            }
+        ])
+        .then
+            //selected employee
+            inquirer 
+                .prompt([
+                    {
+                        type: "list",
+                        name: "newRole",
+                        message: "What is the employees new role",
+                        choices: "CHOICE ARRAY"
+                    }
+                ])
+        .then
+            //insert new changes into database, return to main
+
+}
+
+
+
+viewAllRoles = () => {
+    db.query("SELECT * FROM roles", (err, results) => {
+        if(err){
+            console.log(err)
+        }
+        console.table(results)
+        console.log("\n")
+    })
+    mainMenu()
+    
+
+}  
+
+viewAllDepartments = () => {
+    db.query("SELECT * FROM department", (err, results) => {
+        if(err){
+            console.log(err)
+        }
+        console.table(results)
+    })
+    mainMenu()
+}
